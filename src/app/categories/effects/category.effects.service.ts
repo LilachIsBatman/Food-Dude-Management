acfc5a4330dd597e0ffb2730279836a651151500
LilachIsBatman <lilach369@gmail.com>
2021-03-03 21:01:02 +0200

    user authorizationService and get token from the store

--- src/app/categories/effects/category.effects.service.ts
+++ src/app/categories/effects/category.effects.service.ts
@@ -2,49 +2,61 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Actions, createEffect, ofType, OnInitEffects } from '@ngrx/effects';
 import { Action } from '@ngrx/store';
-import {map, switchMap, switchMapTo} from 'rxjs/operators';
+import { map, switchMap, switchMapTo } from 'rxjs/operators';
 import {
-  createCategory, createCategorySuccess,
-  deleteCategory, deleteCategorySuccess,
-  loadCategories, loadCategoriesSuccess, updateCategory, updateCategorySuccess
+  createCategory,
+  createCategorySuccess,
+  deleteCategory,
+  deleteCategorySuccess,
+  loadCategories,
+  loadCategoriesSuccess,
+  updateCategory,
+  updateCategorySuccess,
 } from '../actions/category.action';
-import {Category} from '../../entity/category.interface';
-import {omit} from 'lodash';
+import { Category } from '../../entity/category.interface';
+import { omit } from 'lodash';
+import { AuthorizationService } from '../../authorization-service';
+import { combineLatest } from 'rxjs';
 
 @Injectable()
 export class CategoryEffects implements OnInitEffects {
-  constructor(private actions: Actions, private http: HttpClient) {}
+  constructor(
+    private actions: Actions,
+    private http: HttpClient,
+    private authorizationService: AuthorizationService
+  ) {}
 
   loadCategories$ = createEffect(() =>
-    this.actions.pipe(
-      ofType(loadCategories),
-      switchMapTo(
-        this.http.get('https://food-dude.herokuapp.com/Categories', {
-          headers: {
-            Authorization:
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
-          },
-        })
+    combineLatest([
+      this.authorizationService.getToken$(),
+      this.actions.pipe(ofType(loadCategories)),
+    ]).pipe(
+      switchMap(([token]) =>
+        this.http.get<Category[]>(
+          'https://food-dude.herokuapp.com/Categories',
+          {
+            headers: {
+              Authorization: token,
+            },
+          }
+        )
       ),
-      map((categories: Category[]) => loadCategoriesSuccess({ categories }))
+      map((categories) => loadCategoriesSuccess({ categories }))
     )
   );
 
   deleteCategory$ = createEffect(() =>
     this.actions.pipe(
       ofType(deleteCategory),
-      switchMap(({id}) =>
-        this.http.delete(
-          `https://food-dude.herokuapp.com/Categories/${id}`,
-          {
-            headers: {
-              Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
-            },
-          }
-        )
+      switchMap(({ id }) =>
+        this.http.delete(`https://food-dude.herokuapp.com/Categories/${id}`, {
+          headers: {
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
+          },
+        })
       ),
-      map((category: Category) => deleteCategorySuccess({category}))
+      map((category: Category) => deleteCategorySuccess({ category }))
     )
   );
 
--- src/app/users/effects/user.effects.service.ts
+++ src/app/users/effects/user.effects.service.ts
@@ -10,23 +10,37 @@ import {
   updateUser,
   updateUserSuccess,
 } from '../actions/user.actions';
-import {Action} from '@ngrx/store';
+import { Action, select, Store } from '@ngrx/store';
 import { map, switchMap, switchMapTo } from 'rxjs/operators';
 import { User } from '../../entity/user.interface';
 import { omit } from 'lodash';
+import { UserState } from '../reducer/user.reducer';
+import { getErrorMassage } from '../../login/selectors/auth.selectors';
+import { AuthorizationService } from '../../authorization-service';
+import { combineLatest } from 'rxjs';
+import {
+  loadCategories,
+  loadCategoriesSuccess,
+} from '../../categories/actions/category.action';
+import { Category } from '../../entity/category.interface';
 
 @Injectable()
 export class UserEffects implements OnInitEffects {
-  constructor(private actions: Actions, private http: HttpClient) {}
+  constructor(
+    private actions: Actions,
+    private http: HttpClient,
+    private authorizationService: AuthorizationService
+  ) {}
 
   loadUsers$ = createEffect(() =>
-    this.actions.pipe(
-      ofType(loadUsers),
-      switchMapTo(
-        this.http.get('https://food-dude.herokuapp.com/users', {
+    combineLatest([
+      this.authorizationService.getToken$(),
+      this.actions.pipe(ofType(loadUsers)),
+    ]).pipe(
+      switchMap(([token]) =>
+        this.http.get<User[]>('https://food-dude.herokuapp.com/users', {
           headers: {
-            Authorization:
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
+            Authorization: token,
           },
         })
       ),
