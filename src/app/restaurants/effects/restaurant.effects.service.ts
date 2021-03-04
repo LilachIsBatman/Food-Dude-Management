c3d32920c184d269126334453a1744494053c9ba
LilachIsBatman <lilach369@gmail.com>
2021-03-04 16:20:00 +0200

    add authorization to restaurant

--- src/app/restaurants/effects/restaurant.effects.service.ts
+++ src/app/restaurants/effects/restaurant.effects.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Actions, createEffect, ofType, OnInitEffects} from '@ngrx/effects';
 import {Action} from '@ngrx/store';
-import {map, switchMap, switchMapTo} from 'rxjs/operators';
+import {map, switchMap} from 'rxjs/operators';
 import {
   createRestaurant, createRestaurantSuccess,
   deleteRestaurant,
@@ -14,20 +14,23 @@ import {
 } from '../actions/restaurant.action';
 import {Restaurant} from '../../entity/restaurant.interface';
 import {omit} from 'lodash';
+import {combineLatest} from 'rxjs';
+import {AuthorizationService} from '../../authorization-service';
 
 @Injectable()
 export class RestaurantEffects implements OnInitEffects {
-  constructor(private actions: Actions, private http: HttpClient) {
+  constructor(private actions: Actions, private http: HttpClient, private authorizationService: AuthorizationService) {
   }
 
   loadRestaurants$ = createEffect(() =>
-    this.actions.pipe(
-      ofType(loadRestaurants),
-      switchMapTo(
+    combineLatest([
+      this.authorizationService.getToken$(),
+      this.actions.pipe(ofType(loadRestaurants)),
+    ]).pipe(
+      switchMap(([token]) =>
         this.http.get('https://food-dude.herokuapp.com/restaurants', {
           headers: {
-            Authorization:
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
+            Authorization: token
           },
         })
       ),
@@ -38,16 +41,17 @@ export class RestaurantEffects implements OnInitEffects {
   );
 
   updateRestaurant$ = createEffect(() =>
-    this.actions.pipe(
-      ofType(updateRestaurant),
-      switchMap(({update}) =>
+    combineLatest([
+      this.authorizationService.getToken$(),
+      this.actions.pipe(ofType(updateRestaurant)),
+    ]).pipe(
+      switchMap(([token, {update}]) =>
         this.http.put(
           `https://food-dude.herokuapp.com/restaurants/${update._id}`,
           omit({...update, category: update.category._id}, ['_id', 'reviewsBlocked']),
           {
             headers: {
-              Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
+              Authorization: token
             },
           }
         )
@@ -57,15 +61,16 @@ export class RestaurantEffects implements OnInitEffects {
   );
 
   deleteRestaurant$ = createEffect(() =>
-    this.actions.pipe(
-      ofType(deleteRestaurant),
-      switchMap(({id}) =>
+    combineLatest([
+      this.authorizationService.getToken$(),
+      this.actions.pipe(ofType(deleteRestaurant)),
+    ]).pipe(
+      switchMap(([token, {id}]) =>
         this.http.delete(
           `https://food-dude.herokuapp.com/Restaurants/${id}`,
           {
             headers: {
-              Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
+              Authorization: token
             },
           }
         )
@@ -75,14 +80,15 @@ export class RestaurantEffects implements OnInitEffects {
   );
 
   searchRestaurants$ = createEffect(() =>
-    this.actions.pipe(
-      ofType(searchRestaurants),
-      switchMap(({params}: { params: any }) =>
+    combineLatest([
+      this.authorizationService.getToken$(),
+      this.actions.pipe(ofType(searchRestaurants)),
+    ]).pipe(
+      switchMap(([token, {params}]: [string, any]) =>
         this.http.get('https://food-dude.herokuapp.com/restaurants/search', {
           params,
           headers: {
-            Authorization:
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
+            Authorization: token
           },
         })
       ),
@@ -91,16 +97,17 @@ export class RestaurantEffects implements OnInitEffects {
   );
 
   createRestaurant$ = createEffect(() =>
-    this.actions.pipe(
-      ofType(createRestaurant),
-      switchMap(({ create }) =>
+    combineLatest([
+      this.authorizationService.getToken$(),
+      this.actions.pipe(ofType(createRestaurant)),
+    ]).pipe(
+      switchMap(([token, { create }]) =>
         this.http.post(
           `https://food-dude.herokuapp.com/Restaurants`,
           omit({...create, category: create.category._id}, ['_id', 'reviewsBlocked', 'reviews']),
           {
             headers: {
-              Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZmQ2ODBhODgzZWY0YzA2MzQ5NWFhNDMiLCJlbWFpbCI6InRvbUBnbWFpbC5jb20iLCJmaXJzdE5hbWUiOiJ0b20iLCJsYXN0TmFtZSI6InBvcmF0IiwiYWRkcmVzcyI6eyJhcmVhIjoiY2VudGVyIiwiY2l0eSI6IlRlbCBBdml2Iiwic3RyZWV0IjoiS2FwbGFuIiwiaG91c2VOdW1iZXIiOjF9LCJyb2xlIjoiYWRtaW4iLCJpYXQiOjE2MDg5OTA5NTZ9.rmP05WiEaqH80V6KXOaU2-YYIIHr5joX3MFbCreXtYA',
+              Authorization: token
             },
           }
         )
